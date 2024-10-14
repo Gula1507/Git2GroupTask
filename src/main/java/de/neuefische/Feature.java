52b9b50637bbd7d2e48a2ec445c8f467f48ecd99
Gula <gulya@gmx.net>
2024-10-14 15:15:56 +0200

    add stream

--- src/main/java/de/neuefische/Feature.java
+++ src/main/java/de/neuefische/Feature.java
@@ -1,7 +1,14 @@
 package de.neuefische;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Feature {
     public static void main(String[] args) {
         System.out.println("Hier ist was neues");
+        List<Integer> numbers=new ArrayList<>(List.of(1,2,3,71));
+        List<Double> newNumbers=numbers.stream().map(n->Double.valueOf(n)/10).collect(Collectors.toList());
+        System.out.println(newNumbers);
     }
 }
