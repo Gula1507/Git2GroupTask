b6bfa3c0064c5cb828b510ff518e0d26c98595a7
Gula <gulya@gmx.net>
2024-10-14 15:18:42 +0200

    add AnotherFeature - add BigDecimal

---
+++ src/main/java/de/neuefische/AnotherFeature.java
@@ -0,0 +1,10 @@
+package de.neuefische;
+
+import java.math.BigDecimal;
+
+public class AnotherFeature {
+    public static void main(String[] args) {
+        BigDecimal bigDecimal=new BigDecimal("1.20");
+
+    }
+}
